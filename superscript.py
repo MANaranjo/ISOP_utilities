fb4af1ba126a5f52e97dabf9a35fbec860d306e4
Miguel Ángel Naranjo Ortiz <MANaranjo@users.noreply.github.com>
2022-05-04 14:51:30 +0200

    Updated superscript.py
    Superscript now extracts the sequences automatically

--- superscript.py
+++ superscript.py
@@ -2,6 +2,8 @@ from Bio import SeqIO
 import sys, argparse, os, re
 import numpy
 import gzip, bz2, tarfile
+import shutil
+from Bio import SeqIO
 
 fastalist = sys.argv[1:]
 
@@ -435,6 +437,47 @@ def parse_conversion_table (table):
 		ctable[chunk[0]] == ctable[chunk[1]]
 	return (cdict)
 
+def pick_longest(fastafile):
+	fa_dict = SeqIO.parse(fastafile, "fasta")
+	longest = ""
+	for i in fa_dict:
+		if len(i.seq) > len(longest):
+			longest = str(i.seq)
+	return(longest)
+
+def parse_superscript(dirinput):
+	if dirinput[-1] != "/":
+		dirinput = dirinput + "/"
+	outdir = dirinput+"parsed_seqs/"
+	if os.path.exists(outdir) == True:
+		shutil.rmtree(outdir)
+	os.makedirs(outdir)
+	for d in os.listdir(dirinput):
+		if os.path.isdir(d) == True:
+			subdir = dirinput+d+"/"
+			for f in os.listdir(subdir):
+				if f.find("filtered_contigs.fa") > -1:
+					longest = pick_longest(subdir+f)
+					a = f.split(".txt.")
+					samplename = d 
+					seqname = a[1].replace(samplename+"_", "")
+					myfasta = outdir + seqname.split(".")[0] + ".fasta"
+					
+					if os.path.exists(myfasta) == True:
+						outfile = open(myfasta, "a")
+						outfile.write(">"+samplename+"\n")
+						outfile.write(longest)
+						outfile.write("/n")
+						outfile.close()
+					else:
+						outfile = open(myfasta, "w")
+						outfile.write(">"+samplename+"\n")
+						outfile.write(longest)
+						outfile.write("/n")
+						outfile.close()
+						continue
 
 prep, output_report = preparation(args.libraries, 100, directory)
 arrange_files(prep, args.table, args.conda, directory, aTRAM, HybPiper, bait)
+parse_superscript(directory)
+
